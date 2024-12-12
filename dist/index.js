be07ab30db293b7be079e054586a1a21a3a7607d
Alon Farchy <34091646+afarchy@users.noreply.github.com>
2024-12-11 19:37:11 -0600

    activate-unity-license@v1.0.5 (#6)
    - Support loading user/pass from environment variables.

--- dist/index.js
+++ dist/index.js
@@ -28598,6 +28598,7 @@ exports["default"] = _default;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.Activate = Activate;
+const process_1 = __nccwpck_require__(7282);
 const licenseClient = __nccwpck_require__(8447);
 const core = __nccwpck_require__(2186);
 async function Activate() {
@@ -28627,9 +28628,24 @@ async function Activate() {
                 await licenseClient.ActivateLicenseWithConfig(servicesConfig);
             }
             else {
-                const username = core.getInput('username', { required: true }).trim();
-                const password = core.getInput('password', { required: true }).trim();
-                const serial = core.getInput('serial', { required: license.toLowerCase().startsWith('pro') });
+                const pro = license.toLowerCase().startsWith('pro');
+                let username = core.getInput('username', { required: pro }).trim();
+                let password = core.getInput('password', { required: pro }).trim();
+                const serial = core.getInput('serial', { required: pro });
+                if (!username) {
+                    const encodedUsername = process_1.env['UNITY_USERNAME_BASE64'];
+                    if (!encodedUsername) {
+                        throw Error('Username is required for Unity License Activation!');
+                    }
+                    username = Buffer.from(encodedUsername, 'base64').toString('utf-8');
+                }
+                if (!password) {
+                    const encodedPassword = process_1.env['UNITY_PASSWORD_BASE64'];
+                    if (!encodedPassword) {
+                        throw Error('Password is required for Unity License Activation!');
+                    }
+                    password = Buffer.from(encodedPassword, 'base64').toString('utf-8');
+                }
                 await licenseClient.ActivateLicense(username, password, serial);
             }
             activeLicenses = await licenseClient.ShowEntitlements();
@@ -29088,6 +29104,14 @@ module.exports = require("path");
 
 "use strict";
 module.exports = require("perf_hooks");
+
+/***/ }),
+
+/***/ 7282:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("process");
 
 /***/ }),
 
--- src/activate.ts
+++ src/activate.ts
@@ -1,3 +1,4 @@
+import { env } from 'process';
 import licenseClient = require('./licensing-client');
 import core = require('@actions/core');
 
@@ -27,9 +28,29 @@ async function Activate(): Promise<void> {
                 const servicesConfig = core.getInput('services-config', { required: true });
                 await licenseClient.ActivateLicenseWithConfig(servicesConfig);
             } else {
-                const username = core.getInput('username', { required: true }).trim();
-                const password = core.getInput('password', { required: true }).trim();
-                const serial = core.getInput('serial', { required: license.toLowerCase().startsWith('pro') });
+                const pro = license.toLowerCase().startsWith('pro');
+                let username = core.getInput('username', { required: pro }).trim();
+                let password = core.getInput('password', { required: pro }).trim();
+                const serial = core.getInput('serial', { required: pro });
+
+                if (!username) {
+                    const encodedUsername = env['UNITY_USERNAME_BASE64'];
+                    if (!encodedUsername) {
+                        throw Error('Username is required for Unity License Activation!');
+                    }
+
+                    username = Buffer.from(encodedUsername, 'base64').toString('utf-8');
+                }
+
+                if (!password) {
+                    const encodedPassword = env['UNITY_PASSWORD_BASE64'];
+                    if (!encodedPassword) {
+                        throw Error('Password is required for Unity License Activation!');
+                    }
+
+                    password = Buffer.from(encodedPassword, 'base64').toString('utf-8');
+                }
+
                 await licenseClient.ActivateLicense(username, password, serial);
             }
             activeLicenses = await licenseClient.ShowEntitlements();
