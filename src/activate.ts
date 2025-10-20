2fed7b03fc2ced9df4beb7967745bd782c114132
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-20 15:23:37 -0400

    activate-unity-license@v2.0.0 (#14)
    - Refactored action to use unity-cli@v1.4.1

--- src/activate.ts
+++ src/activate.ts
@@ -1,23 +1,20 @@
 import core = require('@actions/core');
-import { env } from 'process';
 import {
-    ActivateLicense,
-    ActivateLicenseWithConfig,
-    ShowEntitlements,
-    Version,
-} from './licensing-client';
-import { LicenseType } from './types';
+    LicenseType,
+    LicensingClient,
+} from '@rage-against-the-pixel/unity-cli';
 
 export async function Activate(): Promise<void> {
-    let license: LicenseType | undefined = undefined;
+    let licensingClient: LicensingClient;
+    let licenseType: LicenseType | undefined = undefined;
 
     try {
         core.saveState('isPost', true);
-        await Version();
+        licensingClient = new LicensingClient(core.getInput('license-version'));
         const licenseInput = core.getInput('license', { required: true });
-        license = licenseInput.toLowerCase() as LicenseType;
+        licenseType = licenseInput.toLowerCase() as LicenseType;
 
-        switch (license) {
+        switch (licenseType) {
             case LicenseType.professional:
             case LicenseType.personal:
             case LicenseType.floating:
@@ -26,27 +23,31 @@ export async function Activate(): Promise<void> {
                 throw Error(`Invalid License: ${licenseInput}! Must be one of: ${Object.values(LicenseType).join(', ')}`);
         }
 
-        core.saveState('license', license);
-        let activeLicenses = await ShowEntitlements();
+        core.saveState('license', licenseType);
+        let activeLicenses = await licensingClient.GetActiveEntitlements();
 
-        if (activeLicenses.includes(license)) {
-            core.info(`Unity ${license} License already activated!`);
+        if (activeLicenses.includes(licenseType)) {
+            core.info(`Unity ${licenseType} License already activated!`);
             process.exit(0);
         }
 
         core.startGroup('Attempting to activate Unity License...');
 
         try {
-            if (license === LicenseType.floating) {
-                const servicesConfig = core.getInput('services-config', { required: true });
-                await ActivateLicenseWithConfig(servicesConfig);
+            let servicesConfig: string | undefined = undefined;
+            let username: string | undefined = undefined;
+            let password: string | undefined = undefined;
+            let serial: string | undefined = undefined;
+
+            if (licenseType === LicenseType.floating) {
+                servicesConfig = core.getInput('services-config', { required: true });
             } else {
-                let username = core.getInput('username', { required: false }).trim();
-                let password = core.getInput('password', { required: false }).trim();
-                const serial = core.getInput('serial');
+                username = core.getInput('username', { required: false }).trim();
+                password = core.getInput('password', { required: false }).trim();
+                serial = core.getInput('serial');
 
                 if (!username) {
-                    const encodedUsername = env['UNITY_USERNAME_BASE64'];
+                    const encodedUsername = process.env.UNITY_USERNAME_BASE64;
 
                     if (!encodedUsername) {
                         throw Error('Username is required for Unity License Activation!');
@@ -62,7 +63,7 @@ export async function Activate(): Promise<void> {
                 }
 
                 if (!password) {
-                    const encodedPassword = env['UNITY_PASSWORD_BASE64'];
+                    const encodedPassword = process.env.UNITY_PASSWORD_BASE64;
 
                     if (!encodedPassword) {
                         throw Error('Password is required for Unity License Activation!');
@@ -74,20 +75,19 @@ export async function Activate(): Promise<void> {
                 if (password.length === 0) {
                     throw Error('Password is required for Unity License Activation!');
                 }
-
-                await ActivateLicense(license, username, password, serial);
             }
 
-            activeLicenses = await ShowEntitlements();
+            await licensingClient.Activate({ licenseType, servicesConfig, serial, username, password });
+            activeLicenses = await licensingClient.GetActiveEntitlements();
 
-            if (!activeLicenses.includes(license)) {
-                throw Error(`Failed to activate Unity License with ${license}!`);
+            if (!activeLicenses.includes(licenseType)) {
+                throw Error(`Failed to activate Unity License with ${licenseType}!`);
             }
         } finally {
             core.endGroup();
         }
 
-        core.info(`Unity ${license} License Activated!`);
+        core.info(`Unity ${licenseType} License Activated!`);
     } catch (error) {
         core.setFailed(`Unity License Activation Failed!\n${error}`);
         process.exit(1);
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export enum LicenseType {
-    personal = 'personal',
-    professional = 'professional',
-    floating = 'floating'
-}
